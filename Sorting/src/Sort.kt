47a66595aa4d9f8c6106baa5e58fc2be794cfad9
Davidodari <davidkibzodari@gmail.com>
2019-07-08 18:07:26 +0300

    Generic sorting interface

---
+++ Sorting/src/Sort.kt
@@ -0,0 +1,25 @@
+/**
+ *
+ * Provides Generic Interface for Sorting Algorithms
+ *
+ */
+interface Sort {
+
+    /**
+     *
+     * @param unsorted an array of items which are to be sorted in a specified order
+     * @return a sorted array of items from the unsorted list
+     *
+     */
+    fun <T : Comparable<T>> sort(unsorted: Array<T>): Array<T>
+
+    /**
+     *
+     * Provides an alternative to working with arrays
+     *
+     * @param unsorted a list of items which are to be sorted in a specified order
+     * @return a sorted list of items from the unsorted list
+     *
+     */
+    fun <T : Comparable<T>> sort(unsorted: List<T>): List<T>
+}
